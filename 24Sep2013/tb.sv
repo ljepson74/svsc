112369469ba2f262ae0de6ce0c766f11c4d0d1b5
X <X@svsc.com>
2013-09-24 20:04:06 -0700

    Added wires example

---
+++ 24Sep2013/tb.sv
@@ -0,0 +1,24 @@
+module a(input [3:0] d,  output m);
+   assign m = &d;
+endmodule
+
+
+module yyy(input a,output [3:0] d);
+   assign d = {~a, 3'd0};
+endmodule
+
+
+module tb();
+   wire a;
+   assign a = 0;
+
+//   wire [3:0] d;
+//   wire d;
+
+   yyy yyy0(.a(a), .d(d));
+   a a0(.d(d), .m(m));
+
+   initial
+     $monitor("hello %x", m);
+
+endmodule
